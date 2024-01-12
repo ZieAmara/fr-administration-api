e128fb1151da04b34261b3e6c9ac848341115bdb
Zié Amara <zie.traore18@inphb.ci>
2024-01-12 13:32:08 +0100

    refactoring

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,6 @@ export class AppController {
 
     @Get()
     getHello(): string {
-        return 'Hello and welcome to Associations API! Please use http://localhost:3000/api to access the API.';
+        return 'Hello and welcome to Associations API!';
     }
 }
